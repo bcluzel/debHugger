cdf07d7e61045e6ea6e009259e542d0bf5703356
Ho Viet Khang Le <vklho@enseirb-matmeca.fr>
2019-03-23 20:52:14 +0100

    maj

--- app/store/debhugger.js
+++ app/store/debhugger.js
@@ -1,5 +1,27 @@
 export const baseState = {
-  active: 0
+  active: 0,
+  instructions: [
+    'a = 0',
+    'a++',
+    'b = a + 4',
+    {
+      name: 'for in in [1-5]',
+      active: 0,
+      times: 5,
+      instructions: [
+        'a++',
+        'b--'
+      ]
+    }
+  ],
+  memories: [{
+    a: 0
+  }, {
+    a: 1
+  }, {
+    b: 5,
+    a: 1
+  }]
 }
 
 export const state = () => (baseState)
@@ -9,7 +31,12 @@ export const mutations = {
     state.active = value
   },
   nextStep (state) {
-    state.active++
+    if (typeof state.instructions[state.active] === 'object') {
+      state.instructions[state.active].active++
+      state.instructions[state.active].active %= state.instructions[state.active].instructions.length
+    } else {
+      state.active++
+    }
   }
 }
 
