d1204fab887999490d8f74d5a8abad01adfe1204
Lucas Henry <lucas.henry@enseirb-matmeca.fr>
2019-03-23 16:00:17 +0100

    Added function structure

---
+++ check_table.js
@@ -0,0 +1,34 @@
+/* 
+ * File containing fucntions that need to be implemented
+ * */
+
+// Global varibles
+
+var sourceCode = `
+i = 1
+`;
+
+
+
+// @param memory : dictionnaire
+function check_memory(memory) {
+	if (dict_equals(memory, current_instruction_answer)) {
+		next_instruction()
+	}
+}
+
+// TODO : implement
+function next_instruction() {
+
+}
+
+function dict_equals(dict1, dict2) {
+	return True;
+}
+
+/* 
+ * Tell the player he won.
+ * */
+function finish() {
+
+}
