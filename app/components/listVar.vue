60cb81fd0cfa582cf5e7a3de1ca7da4dc0bc045b
Lucas Henry <lucas.henry@enseirb-matmeca.fr>
2019-03-24 10:35:44 +0100

    Refactored display

---
+++ app/components/listVar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div title={{ listName }}>
+        <!-- Ajouter un v-bind -->
+
+        <LRow v-for="(value, i) in vars_comp">
+            <v-text-field outline :label="i.toString()" :value="value" @input="$emit('input', $event)"> </v-text-field>
+            <!-- <v-btn class="xs6" round color="info" block=true>
+                <font color="back" style="bold">
+                    {{ varName }}
+                </font>
+            </v-btn>
+            <v-btn class="xs6" round color="warning" block=true>
+                {{ varValue }}
+            </v-btn>
+            -->
+            <v-btn color="error" icon @click="$emit('click_deletion')">
+                <v-icon>
+                    delete
+                </v-icon>
+            </v-btn> 
+        </LRow>
+    </div>
+</template>
+
+
+<script>
+import Variable from '@/components/var'
+
+export default {
+    name: "BoiteVariable",
+    components: {
+        'var-comp': Variable
+    },
+    props: {
+        listName: {
+            type: String,
+            required: true
+        },
+        vars_comp: {
+            type: Array,
+            required: true
+        }
+    },
+    event: 'click_deletion'
+}
+</script>
---
+++ app/components/var.vue
@@ -0,0 +1,38 @@
+<template>
+    <LRow>
+        <v-text-field outline :label="varName" :value="varValue" @input="$emit('input', $event)"> </v-text-field>
+        <!-- <v-btn class="xs6" round color="info" block=true>
+            <font color="back" style="bold">
+                {{ varName }}
+            </font>
+        </v-btn>
+        <v-btn class="xs6" round color="warning" block=true>
+            {{ varValue }}
+        </v-btn>
+        -->
+        <v-btn color="error" icon @click="$emit('click_deletion')">
+            <v-icon>
+                delete
+            </v-icon>
+        </v-btn> 
+    </LRow>
+</template>
+
+<script>
+export default {
+    name: "BoiteVariable",
+    props: {
+        varName: {
+            type: String,
+            required: true,
+            validator (value) {
+                return true;
+            }
+        },
+        varValue: {
+            required: true,
+        }
+    },
+    event: 'click_deletion'
+}
+</script>
